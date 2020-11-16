3eac56327cdea6a1533c3be5ec1864a792fd8eb9
Jacky <jacky85031085@gmail.com>
2020-11-16 17:31:13 +0900

    add main file

---
+++ chat.py
@@ -0,0 +1,27 @@
+message = []
+new_message = []
+
+def read_file(file):
+	with open(file, "r", encoding = "utf-8-sig") as chat_file:
+		for line in chat_file:
+			message.append(line.strip())
+
+def change_message(message):
+	name = ""
+	for line in message:
+		if line == "Allen" or line == "Tom":
+			name = line
+			continue
+		if name != None:
+			new_message.append(name + ": " + line)
+
+def show(new_message):
+	for line in new_message:
+		print(line)
+
+def main():
+	read_file("input.txt")
+	change_message(message)
+	show(new_message)
+
+main()
